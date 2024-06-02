7a275f0a50a978cb6da31b948a28a22a8abbf794
slonopot <jesuisledebut@gmail.com>
2024-06-03 01:20:52 +0300

    Schema fix

--- Museexmatch/Plugin.cs
+++ Museexmatch/Plugin.cs
@@ -32,7 +32,7 @@ namespace MusicBeePlugin
             info.Type = PluginType.LyricsRetrieval;
             info.VersionMajor = 1;
             info.VersionMinor = 0;
-            info.Revision = 3;
+            info.Revision = 4;
             info.MinInterfaceVersion = 20;
             info.MinApiRevision = 25;
             info.ReceiveNotifications = ReceiveNotificationFlags.StartupOnly;
